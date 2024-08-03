159381c090e8951e0a53eedebd3eada93bb4e656
Jyoti01-Singh <jyotisingh15072004@gmail.com>
2024-08-03 18:34:22 +0530

    Add files via upload

---
+++ CampusPlacement.py
@@ -0,0 +1,117 @@
+import pandas as pd
+data = pd.read_csv('Placement.csv')
+import warnings
+warnings.filterwarnings('ignore')
+data.head()
+data.tail()
+new_var =data.shape
+new_var
+print("Number of Rows",data.shape[0])
+print("Number of Columns",data.shape[1])
+data.info()
+data.isnull().sum()
+data.describe()
+data.columns
+data['status'].unique()
+data['status'].value_counts()
+data.columns
+data[(data['degree_t']=="Sci&Tech") & (data['status']=="Placed")].sort_values(by="salary",ascending=False).head()
+data.head()
+data = data.drop(['sl_no','salary'],axis=1)
+data.head(1)
+data['ssc_b'].unique()
+data['ssc_b'] = data['ssc_b'].map({'Central':1,'Others':0})
+data.head(2)
+data['hsc_b'].unique()
+data['hsc_b'] = data['hsc_b'].map({'Central':1,'Others':0})
+data.head(2)
+data['hsc_s'].unique()
+data['hsc_s'] = data['hsc_s'].map({'Science':2,'Commerce':1,'Arts':0})
+data.head()
+data['degree_t'].unique()
+data['degree_t'] = data['degree_t'].map({'Sci&Tech':2,'Comm&Mgmt':1,'Others':0})
+data.head(2)
+data['specialisation'].unique()
+data['specialisation'] =data['specialisation'].map({'Mkt&HR':1,'Mkt&Fin':0})
+data.head(2)
+data['workex'].unique()
+data['workex'] = data['workex'].map({'Yes':1,'No':0})
+data.head(2)
+data['status'].unique()
+data['status'] = data['status'].map({'Placed':1,'Not Placed':0})
+data.head()
+data.columns
+X = data.drop('status',axis=1)
+y= data['status']
+y
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20,random_state=42)
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+lr = LogisticRegression()
+lr.fit(X_train,y_train)
+svm = svm.SVC()
+svm.fit(X_train,y_train)
+knn=KNeighborsClassifier()
+knn.fit(X_train,y_train)
+dt=DecisionTreeClassifier()
+dt.fit(X_train,y_train)
+rf=RandomForestClassifier()
+rf.fit(X_train,y_train)
+gb=GradientBoostingClassifier()
+gb.fit(X_train,y_train)
+y_pred1 = lr.predict(X_test)
+y_pred2 = svm.predict(X_test)
+y_pred3 = knn.predict(X_test)
+y_pred4 = dt.predict(X_test)
+y_pred5 = rf.predict(X_test)
+y_pred6 = gb.predict(X_test)
+from sklearn.metrics import accuracy_score
+score1=accuracy_score(y_test,y_pred1)
+score2=accuracy_score(y_test,y_pred2)
+score3=accuracy_score(y_test,y_pred3)
+score4=accuracy_score(y_test,y_pred4)
+score5=accuracy_score(y_test,y_pred5)
+score6=accuracy_score(y_test,y_pred6)
+print(score1,score2,score3,score4,score5,score6)
+final_data = pd.DataFrame({'Models':['LR','SVC','KNN','DT','RF','GB'],
+            'ACC':[score1*100,
+                  score2*100,
+                  score3*100,
+                  score4*100,
+                  score5*100,score6*100]})
+final_data
+import seaborn as sns
+sns.barplot(final_data['Models'],final_data['ACC'])
+new_data = pd.DataFrame({
+    'gender':0,
+    'ssc_p':67.0,
+    'ssc_b':0,
+    'hsc_p':91.0,
+    'hsc_b':0,
+    'hsc_s':1,
+    'degree_p':58.0,
+    'degree_t':2,
+    'workex':0,
+    'etest_p':55.0,
+     'specialisation':1,
+    'mba_p':58.8,   
+},index=[0])
+lr= LogisticRegression()
+lr.fit(X,y)
+p=lr.predict(new_data)
+prob=lr.predict_proba(new_data)
+if p==1:
+    print('Placed')
+    print(f"You will be placed with probability of {prob[0][1]:.2f}")
+else:
+    print("Not-placed")
+prob
+import joblib
+joblib.dump(lr,'model_campus_placement')
+model = joblib.load('model_campus_placement')
+model.predict(new_data)
